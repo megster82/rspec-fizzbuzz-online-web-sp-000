fbfe1f06c2702b5b998fec9357c146a7629adffa
meaghan jennings <meaghanljennings@gmail.com>
2021-02-08 15:06:59 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  elsif int % 3 == 0 
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
   else
-    nil
-  end 
+  end
 end
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
-fizzbuzz(4)
